397ab1564619bd3894567c452797dfe70d1f7739
Derek Hanciles <drhanciles@icloud.com>
2019-03-28 15:36:58 -0600

    Implemented test checking if actions return a type of HAS_ERRORED and the test is passing

--- src/actions/index.test.js
+++ src/actions/index.test.js
@@ -1,17 +1,30 @@
-import * as actions from './index.js'; 
+import * as actions from './index.js';
 
 describe('actions', () => {
   it('should return a type of IS_LOADING', () => {
-    const isLoading = true; 
+    const isLoading = true;
 
     const expectedAction = {
       type: 'IS_LOADING', 
       isLoading
-    }; 
+    };
 
-    const result = actions.isLoading(isLoading); 
+    const result = actions.isLoading(isLoading);
 
-    expect(result).toEqual(expectedAction); 
+    expect(result).toEqual(expectedAction);
 
   }); 
+
+  it('should return a type of HAS_ERRORED', () => {
+    const hasErrored = true;
+
+    const expectedAction = {
+      type: 'HAS_ERRORED', 
+      hasErrored
+    };
+
+    const result = actions.hasErrored = actions.hasErrored(hasErrored);
+
+    expect(result).toEqual(expectedAction);
+  });
 }); 
