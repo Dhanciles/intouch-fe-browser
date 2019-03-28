7fb842614f4e5c8244992eb2e19492fcd295628f
Derek Hanciles <drhanciles@icloud.com>
2019-03-28 13:02:53 -0600

    Updates

---
+++ precache-manifest.6021cf25a8470302fd64ddc36b212239.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ff3ea7cd7731674d8fa0",
+    "url": "/intouch-fe-browser/static/css/main.3f4b5b41.chunk.css"
+  },
+  {
+    "revision": "ff3ea7cd7731674d8fa0",
+    "url": "/intouch-fe-browser/static/js/main.51b32071.chunk.js"
+  },
+  {
+    "revision": "6bc4399e028d1ac3b6f1",
+    "url": "/intouch-fe-browser/static/js/runtime~main.2463fa87.js"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/intouch-fe-browser/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "73cc5233bba39d696a47b85b6b4d5362",
+    "url": "/intouch-fe-browser/index.html"
+  }
+];
