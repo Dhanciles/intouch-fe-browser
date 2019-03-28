68b3997e4666afcd922cfbe98de44d97eb1443a5
Derek Hanciles <drhanciles@icloud.com>
2019-03-28 15:33:18 -0600

    Implemented test checking if action with type of IS_LOADING is being returned and the test is passing

--- src/actions/index.test.js
+++ src/actions/index.test.js
@@ -1,1 +1,17 @@
 import * as actions from './index.js'; 
+
+describe('actions', () => {
+  it('should return a type of IS_LOADING', () => {
+    const isLoading = true; 
+
+    const expectedAction = {
+      type: 'IS_LOADING', 
+      isLoading
+    }; 
+
+    const result = actions.isLoading(isLoading); 
+
+    expect(result).toEqual(expectedAction); 
+
+  }); 
+}); 
