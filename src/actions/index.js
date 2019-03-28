ab3bf41d2e93fd8767f025d3ea332999da6ed1b3
Derek Hanciles <drhanciles@icloud.com>
2019-03-28 15:04:01 -0600

    Implemented the following actions: signOutUser, createContact, getAllContacts

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,9 @@
-export const isLoading = (bool) => ({
+export const isLoading = bool => ({
   type: 'IS_LOADING', 
   isLoading: bool
 })
 
-export const hasErrored = (bool) => ({
+export const hasErrored = bool => ({
   type: 'HAS_ERRORED', 
   hasErrored: bool
 })
@@ -12,4 +12,18 @@ export const signInUser = (userName, token) =>  ({
   type: 'SIGN_IN_USER', 
   userName, 
   token
+})
+
+export const signOutUser = () => ({
+  type: 'SIGN_OUT_USER'
+})
+
+export const createContact = contact => ({
+  type: 'CREATE_CONTACT', 
+  contact
+})
+
+export const getAllContacts = contacts = ({
+  type: 'GET_ALL_CONTACTS', 
+  contacts
 })
