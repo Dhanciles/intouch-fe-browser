24c11685a92e7c313cd4fece98b3e573a9b72d56
Derek Hanciles <drhanciles@icloud.com>
2019-03-28 15:09:44 -0600

    Implemented the following actions: getCurrentContact, updateContact, updateOccasions, getSuggestions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,29 +1,49 @@
 export const isLoading = bool => ({
   type: 'IS_LOADING', 
   isLoading: bool
-})
+}); 
 
 export const hasErrored = bool => ({
   type: 'HAS_ERRORED', 
   hasErrored: bool
-})
+}); 
 
 export const signInUser = (userName, token) =>  ({
   type: 'SIGN_IN_USER', 
   userName, 
   token
-})
+}); 
 
 export const signOutUser = () => ({
   type: 'SIGN_OUT_USER'
-})
+}); 
 
 export const createContact = contact => ({
   type: 'CREATE_CONTACT', 
   contact
-})
+}); 
 
-export const getAllContacts = contacts = ({
+export const getAllContacts = contacts => ({
   type: 'GET_ALL_CONTACTS', 
   contacts
-})
+});
+
+export const getCurrentContact = contact => ({
+  type: 'GET_CURRENT_CONTACT', 
+  contact
+}); 
+
+export const updateContact = contact => ({
+  type: 'UPDATE_CONTACT', 
+  contact
+}); 
+
+export const updateOccasions = occasion => ({
+  type: 'UPDATE_OCCASIONS', 
+  occasion
+}); 
+
+export const getSuggestions = suggestions => ({
+  type: 'GET_SUGGESTIONS', 
+  suggestions
+}); 
